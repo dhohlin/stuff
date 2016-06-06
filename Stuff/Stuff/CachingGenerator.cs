b60bc6282a941d17c14a011d03dbc49b9a62636e
dh <dmitryh@gmail.com>
2016-06-06 22:06:47 +0300

    Added NUnit tests

---
+++ Stuff/Stuff/CachingGenerator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Stuff
+{
+    public class CachingGenerator<T> : IEnumerable<T>
+    {
+        private IEnumerable<T> origGenerator;
+        private IEnumerator<T> origEnumerator;
+
+        private readonly ConcurrentDictionary<int, T> cache = new ConcurrentDictionary<int, T>();
+
+        private CachingGenerator() { }
+
+        public static IEnumerable<T> CreateCachingGenerator(IEnumerable<T> decoratee)
+        {
+            return new CachingGenerator<T>
+            {
+                origGenerator = decoratee
+            };
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CachingEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private bool tryMoveToIndex(int index)
+        {
+            if (cache.ContainsKey(index))
+                return true;
+
+            if (origEnumerator == null)
+                origEnumerator = origGenerator.GetEnumerator();
+            if (!origEnumerator.MoveNext())
+                return false;
+
+            cache.TryAdd(index, origEnumerator.Current);
+            return true;
+        }
+
+        private T getValueAtIndex(int index)
+        {
+            T result;
+            if (!cache.TryGetValue(index, out result))
+                throw new InvalidOperationException("Cache slot is empty");
+            return result;
+        }
+
+        internal bool check()
+        {
+            return true;
+        }
+
+        private class CachingEnumerator : IEnumerator<T>
+        {
+            private readonly CachingGenerator<T> parent;
+            private int index;
+
+            internal CachingEnumerator(CachingGenerator<T> parent)
+            {
+                this.parent = parent;
+                index = -1;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return parent.getValueAtIndex(index);
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return Current;
+                }
+            }
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                index++;
+                return parent.tryMoveToIndex(index);
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
---
+++ Stuff/StuffTests/CachingGeneratorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Stuff.Tests
+{
+    [TestFixture]
+    public class CachingGeneratorTests
+    {
+        private IEnumerable<Guid> getGuidsGenerator()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return Guid.NewGuid();
+            }
+            yield break;
+        }
+
+        [Test]
+        public void SameValuesAfterMultipleEnumerations()
+        {
+            var generator = getGuidsGenerator();
+            var cachingGenerator = CachingGenerator<Guid>.CreateCachingGenerator(generator);
+
+            var firstEnumeration = cachingGenerator.ToArray();
+            var secongEnumeration = cachingGenerator.ToArray();
+            CollectionAssert.AreEqual(firstEnumeration, secongEnumeration);
+        }
+
+        [Test]
+        public void NewEnumeratorAtEachRequest()
+        {
+            var cachingGenerator = CachingGenerator<string>.CreateCachingGenerator(new List<string>() { "qwe", "wer", "ert" });
+            var enumerator1 = cachingGenerator.GetEnumerator();
+            var enumerator2 = cachingGenerator.GetEnumerator();
+            Assert.AreNotSame(enumerator1, enumerator2);
+        }
+
+        [Test]
+        public void SimultaneousIteration()
+        {
+            var generator = getGuidsGenerator();
+            var cachingGenerator = CachingGenerator<Guid>.CreateCachingGenerator(generator);
+
+            var firstEnumerator = cachingGenerator.GetEnumerator();
+            var secondEnumerator = cachingGenerator.GetEnumerator();
+
+            List<Guid> lst1 = new List<Guid>();
+            List<Guid> lst2 = new List<Guid>();
+
+            firstEnumerator.MoveNext();
+            lst1.Add(firstEnumerator.Current);
+            firstEnumerator.MoveNext();
+            lst1.Add(firstEnumerator.Current);
+
+            secondEnumerator.MoveNext();
+            lst2.Add(secondEnumerator.Current);
+
+            firstEnumerator.MoveNext();
+            lst1.Add(firstEnumerator.Current);
+
+            secondEnumerator.MoveNext();
+            lst2.Add(secondEnumerator.Current);
+            secondEnumerator.MoveNext();
+            lst2.Add(secondEnumerator.Current);
+
+            CollectionAssert.AreEqual(lst1, lst2);
+        }
+    }
+}
