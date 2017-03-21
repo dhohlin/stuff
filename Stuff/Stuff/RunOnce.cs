3be68d7c67cac9e33971814cf8cf0f503a952337
dh <dmitryh@gmail.com>
2017-03-21 23:05:34 +0300

    Try another approach with both Action and Func

--- Stuff/Stuff/RunOnce.cs
+++ Stuff/Stuff/RunOnce.cs
@@ -12,12 +12,12 @@ namespace Stuff
     public class RunOnce
     {
         private volatile bool _runBefore = false;
-        private readonly object _locker = new object();        
+        private readonly object _locker = new object();
 
         public void Run(Action action)
         {
             if (_runBefore)
-                return ;
+                return;
             lock (_locker)
             {
                 if (_runBefore)
@@ -27,5 +27,111 @@ namespace Stuff
                 _runBefore = true;
             }
         }
-    }    
+    }
+
+    /// <summary>
+    /// Simple thread safe wrapper for Action which should 
+    /// </summary>
+    public class RunOnceWrapper
+    {
+        private volatile bool _runBefore = false;
+        private readonly object _locker = new object();
+        private Action _action;
+
+        public RunOnceWrapper(Action action)
+        {
+            _action = action;
+        }
+
+        public Action GetAction => run;
+
+        private void run()
+        {
+            if (_runBefore)
+                return;
+            lock (_locker)
+            {
+                if (_runBefore)
+                    return;
+
+                _action();
+                _runBefore = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Simple thread safe wrapper for Action which should 
+    /// </summary>
+    public class RunOnceFuncWrapper<T>
+    {
+        private volatile bool _runBefore = false;
+        private readonly object _locker = new object();
+        private readonly Func<T> _fun;
+
+        public RunOnceFuncWrapper(Func<T> fun)
+        {
+            _fun = () =>
+            {
+                if (_runBefore)
+                    return default(T);
+                lock (_locker)
+                {
+                    if (_runBefore)
+                        return default(T);
+
+                    try
+                    {
+                        return fun();
+                    }
+                    finally
+                    {
+                        _runBefore = true;
+                    }
+                }
+            };
+        }
+
+        public Func<T> GetFunc => _fun;
+    }
+
+    public class RunOnceHelper
+    {
+        private volatile bool _runBefore = false;
+        private readonly object _locker = new object();
+
+        public Func<T> WrapFunc<T>(Func<T> func)
+        {
+            return () =>
+            {
+                if (_runBefore)
+                    return default(T);
+                lock (_locker)
+                {
+                    if (_runBefore)
+                        return default(T);
+
+                    try
+                    {
+                        return func();
+                    }
+                    finally
+                    {
+                        _runBefore = true;
+                    }
+                }
+            };
+        }
+    }
+
+    public static class Ext
+    {
+        public static Func<T> AsRunOnce<T>(this Func<T> func)
+        {
+            var t = func.GetType();
+
+            var helper = new RunOnceHelper();
+            return helper.WrapFunc(func);            
+        }
+    }
 }
--- Stuff/StuffTests/RunOnceTests.cs
+++ Stuff/StuffTests/RunOnceTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Stuff;
@@ -12,7 +13,7 @@ namespace StuffTests
     class RunOnceTests
     {
         [Test]
-        public void SimpleRun()
+        public void TestRunonceInstance()
         {
             var runOnce = new RunOnce();
 
@@ -25,6 +26,49 @@ namespace StuffTests
             runOnce.Run(action);
             runOnce.Run(action);
             Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void TestWrapperWithSimpleAction()
+        {
+            int i = 0;
+            Action action = () => { i++; };
+
+            var runOnce = new RunOnceWrapper(action);
+            var act = runOnce.GetAction;
+
+            act();
+            act();
+            act();
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void TestWrapperWithSingleParameterlessFunc()
+        {
+            int i = 0;
+            Func<bool> func = () =>
+            {
+                i++;
+                return true;
+            };
+
+            var q = func.AsRunOnce();
+            q();
+            q();
+            q();
+            q();
+            q();
+            q();
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void ExamineGenericFunctions()
+        {
+            var func2Type = typeof(Func<int, string, decimal, double, bool>);
+
+            
         }
     }
 }
