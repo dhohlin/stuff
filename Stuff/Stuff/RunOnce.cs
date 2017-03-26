9dfcc091704b5c3bf32d59f69d138c777059da65
dh <dmitryh@gmail.com>
2017-03-26 18:22:49 +0300

    Added extension methods for all generic Action/Func types

--- Stuff/Stuff/RunOnce.cs
+++ Stuff/Stuff/RunOnce.cs
@@ -193,16 +193,42 @@ namespace Stuff
 
     public static class Ext
     {
-        public static Func<T> AsRunOnce<T>(this Func<T> func)
-        {
-            var t = func.GetType();
+        public static Action AsRunOnce(this Action act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1> AsRunOnce<T1>(this Action<T1> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1, T2> AsRunOnce<T1, T2>(this Action<T1, T2> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1, T2, T3> AsRunOnce<T1, T2, T3>(this Action<T1, T2, T3> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1, T2, T3, T4> AsRunOnce<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1, T2, T3, T4, T5> AsRunOnce<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1, T2, T3, T4, T5, T6> AsRunOnce<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1, T2, T3, T4, T5, T6, T7> AsRunOnce<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
+        public static Action<T1, T2, T3, T4, T5, T6, T7, T8> AsRunOnce<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> act)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(act);
 
-            var helper = new RunOnceHelper();
-            return helper.WrapFunc(func);
-        }
-
-        public static Action AsRunOnce(this Action action) => (new RunOnceHelper()).WrapDummy(action);
-        public static Action<T> AsRunOnce<T>(this Action<T> action) => (new RunOnceHelper()).WrapDummy(action);
-        public static Action<T1, T2> AsRunOnce<T1, T2>(this Action<T1, T2> action) => (new RunOnceHelper()).WrapDummy(action);
+        public static Func<TResult> AsRunOnce<TResult>(this Func<TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, TResult> AsRunOnce<T1, TResult>(this Func<T1, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, T2, TResult> AsRunOnce<T1, T2, TResult>(this Func<T1, T2, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, T2, T3, TResult> AsRunOnce<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, T2, T3, T4, TResult> AsRunOnce<T1, T2, T3, T4, TResult>(this Func<T1, T2, T3, T4, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, T2, T3, T4, T5, TResult> AsRunOnce<T1, T2, T3, T4, T5, TResult>(this Func<T1, T2, T3, T4, T5, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, T2, T3, T4, T5, T6, TResult> AsRunOnce<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, T2, T3, T4, T5, T6, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, T2, T3, T4, T5, T6, T7, TResult> AsRunOnce<T1, T2, T3, T4, T5, T6, T7, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> AsRunOnce<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> func)
+            => (new RunOnceHelper()).WrapRunOnceUnsafe(func);
     }
 }
--- Stuff/StuffTests/RunOnceTests.cs
+++ Stuff/StuffTests/RunOnceTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection.Emit;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Stuff;
 
@@ -45,23 +46,157 @@ namespace StuffTests
         }
 
         [Test]
-        public void TestWrapperWithSingleParameterlessFunc()
+        public void TestDecoration_ParameterlessAction()
         {
             int i = 0;
-            Func<bool> func = () =>
+            Action act = () =>
+            {
+                i++;
+            };
+
+            var runOnce = act.AsRunOnce();
+            for (int j = 0; j < 10; j++)
+            {
+                runOnce();
+            }
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void TestDecoration_SingleParameterAction()
+        {
+            int i = 0;
+            Action<bool> act = b =>
+            {
+                i++;
+            };
+
+            var runOnce = act.AsRunOnce();
+            for (int j = 0; j < 10; j++)
+            {
+                runOnce(false);
+            }
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void TestDecoration_TwoParametersAction()
+        {
+            int i = 0;
+            Action<bool, double> act = (b, d) =>
+            {
+                i++;
+            };
+
+            var runOnce = act.AsRunOnce();
+            for (int j = 0; j < 10; j++)
+            {
+                runOnce(false, 23d);
+            }
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void TestDecoration_FiveParametersAction()
+        {
+            int i = 0;
+            Action<int, bool, double, DateTime, object> act = (I, b, d, D, o) =>
+            {
+                i++;
+            };
+
+            var runOnce = act.AsRunOnce();
+            for (int j = 0; j < 10; j++)
+            {
+                runOnce(33, false, 23d, DateTime.Now, new object());
+            }
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void TestDecoration_ParameterlessFunc()
+        {
+            int i = 0;
+            Func<bool> fun = () =>
             {
                 i++;
                 return true;
             };
 
-            var q = func.AsRunOnce();
-            q();
-            q();
-            q();
-            q();
-            q();
-            q();
+            var runOnce = fun.AsRunOnce();
+
+            Assert.AreEqual(true, runOnce());
             Assert.AreEqual(1, i);
+
+            for (int j = 0; j < 10; j++)
+            {
+                Assert.AreEqual(false, runOnce());
+                Assert.AreEqual(1, i);
+            }
+        }
+
+        [Test]
+        public void TestDecoration_SingleParameterFunc()
+        {
+            int i = 0;
+            Func<int, bool> fun = I =>
+            {
+                i++;
+                return true;
+            };
+
+            var runOnce = fun.AsRunOnce();
+
+            Assert.AreEqual(true, runOnce(33));
+            Assert.AreEqual(1, i);
+
+            for (int j = 0; j < 10; j++)
+            {
+                Assert.AreEqual(false, runOnce(j));
+                Assert.AreEqual(1, i);
+            }
+        }
+
+        [Test]
+        public void TestDecoration_TwoParametersFunc()
+        {
+            int i = 0;
+            Func<int, double, bool> act = (I, d) =>
+            {
+                i++;
+                return true;
+            };
+
+            var runOnce = act.AsRunOnce();
+            Assert.AreEqual(true, runOnce(33, 22d));
+            Assert.AreEqual(1, i);
+
+            for (int j = 0; j < 10; j++)
+            {
+                Assert.AreEqual(false, runOnce(j, 73d));
+                Assert.AreEqual(1, i);
+            }
+        }
+
+        [Test]
+        public void TestDecoration_FiveParametersFunc()
+        {
+            int i = 0;
+            Func<int, bool, double, DateTime, object, bool> act = (I, b, d, D, o) =>
+            {
+                i++;
+                return true;
+            };
+
+            var runOnce = act.AsRunOnce();
+            Assert.AreEqual(true, runOnce(33, false, 2d, DateTime.Now, new object()));
+            Assert.AreEqual(1, i);
+
+            for (int j = 0; j < 10; j++)
+            {
+                Assert.AreEqual(false, runOnce(j, false, 2d, DateTime.Now, new object()));
+                Assert.AreEqual(1, i);
+            }
         }
 
         [Test]
@@ -72,7 +207,7 @@ namespace StuffTests
             Assert.AreEqual(true, isEven(2));
             Assert.AreEqual(false, isEven(3));
 
-            var decoreated = (new RunOnceHelper()).WrapDummy(isEven);            
+            var decoreated = (new RunOnceHelper()).WrapDummy(isEven);
             Assert.AreEqual(true, decoreated(2));
             Assert.AreEqual(false, decoreated(3));
         }
@@ -101,6 +236,27 @@ namespace StuffTests
             Assert.AreEqual(false, decorated(3));
             Assert.AreEqual(1, i);
             Assert.AreEqual(false, decorated(3));
+            Assert.AreEqual(1, i);
+        }
+
+        [Test]
+        public void TestMultiThread()
+        {
+            var i = 0;
+            Action act = () => { i++; };
+
+            var runOnce = act.AsRunOnce();
+
+            var evt = new ManualResetEvent(false);
+
+            for (int j = 0; j < 500; j++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    evt.WaitOne();
+                    runOnce();
+                });
+            }
             Assert.AreEqual(1, i);
         }
     }
