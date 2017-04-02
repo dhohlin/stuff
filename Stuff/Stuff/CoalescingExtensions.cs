ce09d78309881c0b2994f81224acb528d294dd03
dh <dmitryh@gmail.com>
2017-04-02 09:48:41 +0300

    Implemented generic coalescing extension method

---
+++ Stuff/Stuff/CoalescingExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Stuff
+{
+    public static class CoalescingExtensions
+    {
+        /// <summary> "Null" coalescing function for all types </summary>        
+        public static T CoalesceWith<T>(this T value, T anotherValue)
+            => value == null || value.Equals(default(T))
+                ? anotherValue
+                : value;
+    }
+}
---
+++ Stuff/StuffTests/CoalescingTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using Stuff;
+
+namespace StuffTests
+{
+    [TestFixture]
+    internal class CoalescingTests
+    {
+        [Test]
+        public void TestCoalescingGuid() => testCoalescing(Guid.Empty, Guid.NewGuid());
+
+        [Test]
+        public void TestCoalescingDateTime() => testCoalescing(default(DateTime), DateTime.Now);
+
+        [Test]
+        public void TestCoalescingBool() => testCoalescing(false, true);
+
+        [Test]
+        public void TestCoalescingInt() => testCoalescing(0, 133);
+
+        [Test]
+        public void TestCoalescingDouble() => testCoalescing(0d, 123.45);
+
+        [Test]
+        public void TestCoalescingString() => testCoalescing(null, "Just a string");
+
+        [Test]
+        public void TestCoalescingCustomStruct()
+            => testCoalescing(new CustomStruct(), new CustomStruct() { I = 333, B = true });
+
+        [Test]
+        public void TestCoalescingCustomClass()
+            => testCoalescing(null, new CustomClass() { I = 333, B = true });
+
+
+        private void testCoalescing<T>(T emptyValue, T nonEmptyValue)
+        {
+            Assert.AreEqual(emptyValue, emptyValue.CoalesceWith(emptyValue));
+            Assert.AreEqual(nonEmptyValue, emptyValue.CoalesceWith(nonEmptyValue));
+            Assert.AreEqual(nonEmptyValue, emptyValue.CoalesceWith(emptyValue).CoalesceWith(emptyValue).CoalesceWith(emptyValue).CoalesceWith(nonEmptyValue));
+        }
+
+        private struct CustomStruct
+        {
+            public int I;
+            public bool B;
+        }
+
+        private class CustomClass
+        {
+            public int I;
+            public bool B;
+        }
+    }
+}
